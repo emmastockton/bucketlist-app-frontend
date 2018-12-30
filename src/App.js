9c55c507e5cc4e5f49d69417990949f1110d4720
emmastockton <emmastockton10@gmail.com>
2018-12-30 20:27:57 +0000

    Adding changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This will be my Bucket List app
           </p>
           <a
             className="App-link"
