40f6e0f3889ceb1438b36f25a9c759b6e82008ca
emmastockton <emmastockton10@gmail.com>
2019-01-02 19:14:11 +0000

    Added InputBox file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import InputBox from './components/InputBox';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
+        <InputBox />
       </div>
     );
   }
---
+++ src/components/InputBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class InputBox extends React.Component {
+
+    render() {
+        return (<input type="text" class="form-control" placeholder="I really want to..."></input>);
+    }
+
+}
+
+const styles = {
+    inputBox : {
+        
+    }
+};
+
+export default InputBox;
