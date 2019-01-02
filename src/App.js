6dc5cb932e51c83b3e5eb3a3555a86964a123744
emmastockton <emmastockton10@gmail.com>
2019-01-02 19:32:49 +0000

    SubmitButton file added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import InputBox from './components/InputBox';
+import SubmitButton from './components/SubmitButton';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
-        <InputBox />
+        <div class="row">
+          <div class="col-sm">
+            <InputBox />
+          </div>
+          <div class="col-sm">
+            <SubmitButton />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/SubmitButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SubmitButton extends React.Component {
+
+    render() {
+        return (<button type="button" class="btn btn-info">Add</button>);
+    }
+
+}
+
+const styles = {
+    submitButton : {
+        
+    }
+};
+
+export default SubmitButton;
