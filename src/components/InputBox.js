d2feb9b78e3b7c0f1101a2854fd3c439ee71029f
emmastockton <emmastockton10@gmail.com>
2019-01-08 21:35:59 +0000

    Added some data handling and can see the text can be passed through

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -6,21 +6,30 @@ class InputBox extends React.Component {
         super(props);
 
         this.state = {
-            entryName: ""
+            entryDescription: ""
         };
 
         this.onSubmitClicked = this.onSubmitClicked.bind(this);
+        this.onEntryFieldUpdated = this.onEntryFieldUpdated.bind(this);
     }
 
     onSubmitClicked () {
-        alert('Hello World');
+        alert(this.state.entryDescription);
+    }
+
+    onEntryFieldUpdated (event) {
+        const entryName = event.target.value;
+
+        this.setState({
+            entryDescription: entryName
+        })
     }
 
     render() {
         return (
             <div className="row">
                 <div className="col">
-                    <input style={styles.InputBox} type="text" class="form-control" placeholder="I really want to..." value={this.state.entryDescription} />
+                    <input style={styles.InputBox} type="text" class="form-control" placeholder="I really want to..." value={this.state.entryDescription} onChange={this.onEntryFieldUpdated} />
                 </div>          
                 <div className="col">
                     <button type="button" class="btn btn-info" onClick={this.onSubmitClicked}>Add</button>
