40f0d903e856428bc87c432b60391a71e5148300
emmastockton <emmastockton10@gmail.com>
2019-01-23 13:44:02 +0000

    Some styling changes to TaskCounter

--- src/components/TaskCounter.js
+++ src/components/TaskCounter.js
@@ -6,26 +6,26 @@ class TaskCounter extends React.Component {
 
         return (
                 <div 
-                    className="containter align-middle" 
+                    className="container" 
                     style={styles.taskCounter} 
                 >
-                <div className="row">
-                <div className="col-2" style={styles.amountToDo} >
+                <div className="row justify-content-md-center">
+                <div className="col-3 text-right" style={styles.amountToDo}>
                     {
                         this.props.tasks.length
                     }
                 </div>
-                    <div className="col-9">
+                    <div className="col-3 text-left">
                         left to do!
                     </div> 
                 </div>
-                <div className="row">
-                <div className="col-2" style={styles.amountDone}>
+                <div className="row justify-content-md-center">
+                <div className="col-3 text-right" style={styles.amountDone}>
                     {
                         this.props.doneTasks.length
                     }                 
                 </div>
-                    <div className="col-9">
+                    <div className="col-3 text-left">
                         already done!
                     </div> 
                 </div>
@@ -37,6 +37,9 @@ class TaskCounter extends React.Component {
 
 const styles = {
     taskCounter : {
+        width: "220px",
+        padding: "25px",
+        margin: "25px",
         border: "solid",
         margin: "auto",
         marginTop: "20px",
@@ -46,11 +49,13 @@ const styles = {
     },
     amountToDo : {
         fontSize: "30px",
-        color: "#ef5811"
+        color: "#ef5811",
+        marginTop: "20px"
     },
     amountDone: {
         fontSize: "30px",
-        color: "#28dc59"
+        color: "#28dc59",
+        marginTop: "5px"
     }
 };
 
